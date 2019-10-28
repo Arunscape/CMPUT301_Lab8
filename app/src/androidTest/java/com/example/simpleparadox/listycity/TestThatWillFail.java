091715ef38963058046d6c473a16d10d19d00a06
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-10-28 15:57:03 -0600

    this test will fail

---
+++ app/src/androidTest/java/com/example/simpleparadox/listycity/TestThatWillFail.java
@@ -0,0 +1,12 @@
+package com.example.simpleparadox.listycity;
+
+import org.junit.Test;
+
+public class TestThatWillFail {
+
+    @Test
+    public void thisTestWillFail(){
+        assert(false);
+        // TODO implement functionality
+    }
+}
