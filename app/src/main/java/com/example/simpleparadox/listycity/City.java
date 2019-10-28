aa016e25d00f5e1ef5bdcaa870e49a3a4379882c
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-10-28 16:17:04 -0600

    idk

--- app/src/main/java/com/example/simpleparadox/listycity/City.java
+++ app/src/main/java/com/example/simpleparadox/listycity/City.java
@@ -34,7 +34,6 @@ public class City implements Comparable<City> {
     }
 
     public String concat() {
-//        return this.city + this.province;
-        return "";
+        return this.city + this.province;
     }
 }
